2ecf952d7cda030659c3041292717144981c9571
Crystal Chen <cry_fairy@CrystaldeMacBook-Pro.local>
2022-09-12 00:47:33 -0400

    try

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override')
 
 var session = require('express-session');
 var passport = require('passport');
@@ -34,6 +35,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'))
 //Configure and Mount Session Middleware for oauth
 app.use(session({
   secret: 'Tradie it!',
